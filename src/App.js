ac620a344d4b5f960ffbe7ae707ff3d0cc28945d
Karlina <107602124+karlinamaya@users.noreply.github.com>
2025-01-28 16:10:57 -0500

    Updated landing page content and design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          DrawSpace
-        </h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <a href="/signin" className="nav-link">Sign In</a>
+        <a href="/register" className="nav-link">Register</a>
+      </nav>
+      <div className="landing-page">
+        <div className="content">
+          <h1>DrawSpace</h1>
+          <p>DrawSpace is a web-based graphics editor for creating, collaborating, and connecting in real-time.</p>
+          <p>Whether brainstorming freely or playing games like "Sketch and Guess," enjoy versatile tools, shared canvases, and a platform for creativity and fun.</p>
+        </div>
+        <div className="image-container">
+          <img src={`${process.env.PUBLIC_URL}/landingpage.jpg`} alt="Landing Page" className="landingpage-image" />
+        </div>
+      </div>
     </div>
   );
 }
