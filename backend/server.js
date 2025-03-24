e27b15e254b6893d85ec0c8feb9589306084c2df
ruben <rubenpierrelouis889@gmail.com>
2025-03-24 15:27:24 -0400

    dynamic server

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(cors())
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB database, create user schema and model
 
