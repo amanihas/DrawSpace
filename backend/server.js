6cf0e5a18c196f5256f9afde284adbd58973c210
ruben <rubenpierrelouis889@gmail.com>
2025-03-30 21:54:42 -0400

    Fixes Es module conflict

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,11 @@ import jwt from 'jsonwebtoken'
 import axios from "axios";
 import multer from 'multer'
 import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 dotenv.config({ path: './backend/.env' });
